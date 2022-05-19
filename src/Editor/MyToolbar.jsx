3edf4a0c7efb3e6e09066299c63ec3ac0164079c
wuhao <13979215061@163.com>
2022-05-19 15:29:25 +0800

    feat: 树形列表按钮响应

--- src/Editor/MyToolbar.jsx
+++ src/Editor/MyToolbar.jsx
@@ -7,10 +7,10 @@ const MyToolbar = () => {
             <Button title={'加粗'} format={'bold'} content={'B'}/>
             <Button title={'斜体'} format={'italic'} content={'I'}/>
             <Button title={'下划线'} format={'underline'} content={'U'}/>
-            <Button title={'无序列表'} format={'bulleted-list'} content={'⨀'}/>
-            <Button title={'有序列表'} format={'numbered-list'} content={'①'}/>
-            <Button title={'标签'} format={'tag'} content={'#'}/>
-            <Button title={'图片'} format={'pic'} content={'🖼'}/>
+            <Button title={'无序列表'} format={'bulleted-list'} content={'⨀'} type={'block'}/>
+            <Button title={'有序列表'} format={'numbered-list'} content={'①'} type={'block'}/>
+            <Button title={'标签'} format={'tag'} content={'#'} type={'block'}/>
+            <Button title={'图片'} format={'pic'} content={'🖼'} type={'block'}/>
         </div>
     )
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
-import { Editor } from 'slate'
+import { Editor, Element as SlateElement, Transforms } from 'slate'
 import { useSlate } from 'slate-react'
 
-// 判断节点属性是否为真
-const isFormatActive = (format, editor) => {
+const LIST_TYPES = ['numbered-list', 'bulleted-list']
+
+// 判断文本属性是否为真
+const isMarkActive = (format, editor) => {
     const marks = Editor.marks(editor)
     return marks ? marks[format] === true : false
 }
 
-// 根据样式切换属性值
-const toggleFormat = (event, editor, format) => {
+// 根据文本样式切换属性值
+const toggleMark = (event, editor, format) => {
     event.preventDefault()
-    const isActive = isFormatActive(format, editor)
+    const isActive = isMarkActive(format, editor)
 
     if (isActive) {
         Editor.removeMark(editor, format)
@@ -21,20 +23,78 @@ const toggleFormat = (event, editor, format) => {
     // Transforms.setNodes(editor, { [format]: !isActive }, { match: n => Text.isText(n), split: true })
 }
 
-const Button = ({ title, content, format }) => {
+// 判断段落属性是否为真
+const isBlockActive = (editor, format, blockType = 'type') => {
+    const { selection } = editor
+    if (!selection) return false
+
+    const [match] = Array.from(
+        Editor.nodes(editor, {
+            at: Editor.unhangRange(editor, selection),
+            match: n =>
+                !Editor.isEditor(n) &&
+                SlateElement.isElement(n) &&
+                n[blockType] === format
+        })
+    )
+    return !!match
+}
+
+// 根据段落样式切换属性值
+const toggleBlock = (event, editor, format) => {
+    event.preventDefault()
+    const isActive = isBlockActive(editor, format, 'type') // 默认左对齐，不做居中
+    const isList = LIST_TYPES.includes(format)
+
+    Transforms.unwrapNodes(editor, {
+        match: n =>
+            !Editor.isEditor(n) &&
+            SlateElement.isElement(n) &&
+            LIST_TYPES.includes(n.type),
+        split: true
+    })
+
+    const newProperties = {
+        type: isActive ? 'paragraph' : isList ? 'list-item' : format
+    }
+    Transforms.setNodes(editor, newProperties)
+
+    if (!isActive && isList) {
+        const block = { type: format, children: [] }
+        Transforms.wrapNodes(editor, block)
+    }
+}
+
+const Button = ({ title, content, format, type = 'mark' }) => {
     const editor = useSlate()
 
-    return (
-        <button
-            title={title}
-            className={isFormatActive(format, editor) ? 'active' : ''}
-            onMouseDown={(event) => {
-                toggleFormat(event, editor, format)
-            }}
-        >
-            {content}
-        </button>
-    )
+    if (type === 'mark') {
+        return (
+            <button
+                title={title}
+                className={isMarkActive(format, editor) ? 'active' : ''}
+                onMouseDown={(event) => {
+                    toggleMark(event, editor, format)
+                }}
+            >
+                {content}
+            </button>
+        )
+    } else if (type === 'block') {
+        return (
+            <button
+                title={title}
+                className={isBlockActive(editor, format) ? 'active' : ''}
+                onMouseDown={(event) => {
+                    toggleBlock(event, editor, format)
+                }}
+            >
+                {content}
+            </button>
+        )
+    } else {
+        return <button>none</button>
+    }
 }
 
 export default Button
