191a2e8aeb332df478785e9f79eef31fb4abd208
wuhao <13979215061@163.com>
2022-08-09 16:36:21 +0800

    feat: 实时预览

--- src/components/Readonly.jsx
+++ src/components/Readonly.jsx
@@ -1,11 +1,24 @@
 import Header from './Header'
 import { createEditor } from 'slate'
 import { Editable, Slate, withReact } from 'slate-react'
-import React, { useMemo } from 'react'
+import React, { useMemo, useCallback } from 'react'
+import Leaf from '../Editor/Leaf'
+import Element from '../Editor/Element'
 
 const Readonly = ({ editorData }) => {
     const editor = useMemo(() => withReact(createEditor()), [])
-    // console.log('props change, but not reRendering', editorData)
+    editor.children = editorData // 强制更新
+
+    // 渲染文本
+    const renderLeaf = useCallback((props) => {
+        return <Leaf {...props}/>
+    }, [])
+
+    // 渲染段落
+    const renderElement = useCallback((props) => {
+        return <Element {...props}/>
+    }, [])
+
     return (
         <div>
             <Header text={'只读编辑器'}/>
@@ -16,6 +29,8 @@ const Readonly = ({ editorData }) => {
                 <Editable
                     className={'editableWrap'}
                     style={{ borderBottom: 'none' }}
+                    renderLeaf={renderLeaf}
+                    renderElement={renderElement}
                     readOnly
                 />
             </Slate>
